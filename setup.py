d54b9964df3e9c93226f50c06519beac2b4bb05a
John James <john.james.ai.studio@gmail.com>
2022-11-06 03:31:15 -0500

    1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Sebastien: Your Fashion Consultant',
+    author='John James',
+    license='BSD-3',
+)
